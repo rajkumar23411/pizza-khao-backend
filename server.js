18046429224033969768347d83947deb095f84bb
Rajkumar Kalita <rajkumarriyan001@gmail.com>
2023-04-25 08:44:43 +0530

    resolving route isse

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(
 );
 app.use(express.static(path.join(__dirname, "public")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.use(express.json());
