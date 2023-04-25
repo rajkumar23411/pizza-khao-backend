4647ea862ed79aaa3672f417f6f11e5755b8a0d6
Rajkumar Kalita <rajkumarriyan001@gmail.com>
2023-04-25 07:59:24 +0530

    fixing

--- server.js
+++ server.js
@@ -32,7 +32,15 @@ process.on("unhandledRejection", (err) => {
 });
 
 dbConnection();
-app.use(cors({ origin: "https://pizzakhao.netlify.app", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "https://pizzakhao.netlify.app",
+      "https://pizza-khao-frontend.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
